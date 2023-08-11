b08bc8c7fc90dfdff86672a5bee5276bbf52f33c
MegMwaura <mwauramegan28@gmail.com>
2023-08-11 16:36:31 -0700

    Correction betty style

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-        int num;
-
-        for (num = 0; num <= 9; num++)
-        {
-                putchar(num + '0');
-        }
-        putchar('\n');
-        return (0);
+	int num;
+	
+	for(num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+	}
+	putchar('\n');
+	return (0);
 }
