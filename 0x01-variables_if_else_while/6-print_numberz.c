9cb7f2ad7498d1ea8a14ac91a8171314da2a3962
MegMwaura <mwauramegan28@gmail.com>
2023-08-11 16:30:09 -0700

    Task.6 numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+        int num;
+
+        for (num = 0; num <= 9; num++)
+        {
+                putchar(num + '0');
+        }
+        putchar('\n');
+        return (0);
+}
