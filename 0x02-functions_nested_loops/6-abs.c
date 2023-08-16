dcc17bed5e90ec6997bf71282771e862011d5ce8
MegMwaura <mwauramegan28@gmail.com>
2023-08-16 15:13:19 -0700

    Computing absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * abs - Computed the absolute value
+ * @n: number to check
+ * Return: absolute value of an integer
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		int abs_val;
+
+		abs_val = n * -1;
+		return (abs_val);
+	}
+	return (n);
+}
