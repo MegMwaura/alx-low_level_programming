77c7fceada75d684cfe045c1c0f5634a1694fc1e
MegMwaura <mwauramegan28@gmail.com>
2023-08-16 14:20:15 -0700

    Correction betty style

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,9 @@
 /**
  * print_sign - Prints the sign of a number
  * @n: checks the number
- * Return: 1 if number is greater than zero, 0 if number is equal to zero , -1 if number is less than zero
+ * Return: 1 if number is greater than zero
+ *  0 if number is equal to zero
+ *  -1 if number is less than zero
  */
 
 int print_sign(int n)
