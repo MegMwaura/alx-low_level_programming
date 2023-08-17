ac6ba3bf56dd5add8a73e7d0252692ac0b5479f6
MegMwaura <mwauramegan28@gmail.com>
2023-08-17 16:23:56 -0700

    Correction, putting space between > and num

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,13 @@
 
 void more_numbers(void)
 {
-	int i;
-	int num;
+	int i, num;
 
 	for (i = 0; i < 10; i++)
 	{
 	for (num = 0; num <= 14; num++)
 	{
-	if (num >10 )
+	if (num > 10)
 	{
 	_putchar((num / 10) + '0');
 	}
