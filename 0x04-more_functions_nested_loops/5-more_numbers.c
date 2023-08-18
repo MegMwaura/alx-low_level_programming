aaf46a3286812c33e1f2458152e98208bc78a0a2
MegMwaura <mwauramegan28@gmail.com>
2023-08-17 19:51:52 -0700

    Prints 10 times the numbers form 0 through 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,7 +6,6 @@
  */
 
 void more_numbers(void)
-
 {
 	int i, num;
 
@@ -14,7 +13,7 @@ void more_numbers(void)
 	{
 	for (num = 0; num <= 14; num++)
 	{
-	if (num > 10)
+	if (num > 9)
 	{
 	_putchar((num / 10) + '0');
 	}
