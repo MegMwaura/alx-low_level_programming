9f8b21724a17d616d750c39a9653cbcaaa7d0c5d
MegMwaura <mwauramegan28@gmail.com>
2023-08-29 11:51:39 -0700

    Function that prints the sum of 2 diagonals of a matrx of integers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,24 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - Entry point of the program
- * @a: input
- * @size: input
- * Return: void
+ * @a: Input
+ * @size: Input
+ * Return: Void
  */
-
 void print_diagsums(int *a, int size)
 {
-	int i;
-	int n;
-	int sum1 = 0;
-	int sum2 = 0;
+	int sum1, sum2, m;
 
-	for (i = 0; i <= (size * size); i = i + size + 1)
+	sum1 = 0;
+	sum2 = 0;
 
-		sum1 = sum1 + a[i];
-
-		for (n == size - 1; n <= (size * size) - size; n = n + size - 1)
-			sum2 = sum2 + a[n];
-		printf("%d, %d\n", sum1, sum2);
+	for (m = 0; m < size; m++)
+	{
+		sum1 = sum1 + a[m * size + m];
+	}
+	for (m = size - 1; m >= 0; m--)
+	{
+		sum2 += a[m * size + (size - m - 1)];
+	}
+	printf("%d,%d\n", sum1, sum2);
 }
