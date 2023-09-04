bf1bc7bd67f1b44f3a1bb9b9325c50cfbbff4422
MegMwaura <mwauramegan28@gmail.com>
2023-09-04 09:28:18 -0700

    Program that multiplies 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,25 +2,30 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer
- * @s: String to be converted
- * Return: Integer from string conversion
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
  */
-
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
 	while (s[len] != '\0')
-	{
 		len++;
-	}
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-		{
 			++d;
-		}
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -28,27 +33,26 @@ int _atoi(char *s)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-			{
 				break;
-				f = 0;
-			}
-			i++;
+			f = 0;
 		}
-		if (f == 0)
-			return (0);
+		i++;
 	}
+
+	if (f == 0)
+		return (0);
+
 	return (n);
 }
 
 /**
- * main - Multiplies two numbers
- * @argc: Number of arguments
+ * main - multiplies two numbers
+ * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 (Success), 1(Error)
+ *
+ * Return: 0 (Success), 1 (Error)
  */
-
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
@@ -58,10 +62,14 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
 
 	printf("%d\n", result);
+
 	return (0);
 }
+
+
