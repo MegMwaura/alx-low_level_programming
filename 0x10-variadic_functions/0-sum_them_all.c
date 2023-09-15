6cfd3d659157d80f08a97862c5a19499b17c316a
MegMwaura <mwauramegan28@gmail.com>
2023-09-15 05:01:41 -0700

    Creating readme files,variadic_functions.h and function that returns the sum of all its parametres

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Returns the sum of all its parametres
+ * @n: Number of parametres passed to the function\
+ * @...: A variable number of parametres to calculate sum of
+ * Return: 0 if n == 0, otherwise return the sum of parametres
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+
+	va_end(ap);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,6 @@
+0x10. C - Variadic functions
+
+Task 0. Function that returns the sum of  all its parametres
+Task 1. Function that prints numebrs followed by a new line
+Task.2 Function that prints strings followed by a new line
+Task 3. A function that prints anything 
