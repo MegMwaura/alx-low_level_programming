ec5ec9e3659c5087d466983feb49427deeb73490
MegMwuara <mwauramegan28@gmail.com>
2023-09-22 14:34:34 -0700

    Correcting betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,22 +10,22 @@
 
 size_t print_list(const list_t *h)
 {
- size_t node_count = 0;
+	size_t node_count = 0;
 
- while (h)
- {
- 	if (!h->str)
+	while (h)
 	{
- 		printf("[0] (nil)\n");
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		node_count++;
 	}
-	else
-	{
-		printf("[%u] %s\n", h->len, h->str);
-	}
-	h = h->next;
-	node_count++;
-}
 
- return (node_count);
+	return (node_count);
 }
 
