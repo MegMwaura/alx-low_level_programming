0deb90be078bf3c6b8eeca3d51efbdad5b5a155d
MegMwaura <mwauramegan28@gmail.com>
2023-11-23 16:32:58 -0800

    Correcting betty indentation in source file"

--- 0x14-bit_manipulation/0-main.c
+++ 0x14-bit_manipulation/0-main.c
@@ -3,6 +3,7 @@
 
 /**
  * main - check the code
+ * @b: Pointer to the string
  *
  * Return: Always 0.
  */
@@ -10,17 +11,17 @@ unsigned int binary_to_uint(const char *b);
 
 int main(void)
 {
-    unsigned int n;
+	unsigned int n;
 
-    n = binary_to_uint("1");
-    printf("%u\n", n);
-    n = binary_to_uint("101");
-    printf("%u\n", n);
-    n = binary_to_uint("1e01");
-    printf("%u\n", n);
-    n = binary_to_uint("1100010");
-    printf("%u\n", n);
-    n = binary_to_uint("0000000000000000000110010010");
-    printf("%u\n", n);
-    return (0);
+	n = binary_to_uint("1");
+	printf("%u\n", n);
+	n = binary_to_uint("101");
+	printf("%u\n", n);
+	n = binary_to_uint("1e01");
+	printf("%u\n", n);
+	n = binary_to_uint("1100010");
+	printf("%u\n", n);
+	n = binary_to_uint("0000000000000000000110010010");
+	printf("%u\n", n);
+	return (0);
 }
